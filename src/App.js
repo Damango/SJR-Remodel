e0d8f8e6057e8e2f5ddcde99fa125b93e49c364e
Justin Kessler <redcoderhd1@gmail.com>
2021-09-22 23:14:07 -0400

    First upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import SJRStateLogo from "./media/sjrstatelogo.png"
+import DropDownLink from "./components/DropDownLink/DropDownLink";
 import './App.css';
 
 function App() {
+
+
+  let lists = [
+    {header: 'STUDENTS',
+  links:['Future Students', 'Current Students','Academic Advising','Academic Calender', 'Admissions APPLY NOW','Book Store', 'Career Services']},
+  {header: 'ACADEMICS',
+  links:['Future Students', 'Current Students','Academic Advising','Academic Calender', 'Admissions APPLY NOW','Book Store', 'Career Services']},
+  {header: 'ATHLETICS',
+  links:['Future Students', 'Current Students','Academic Advising','Academic Calender', 'Admissions APPLY NOW','Book Store', 'Career Services']}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-bar-container">
+        <div className="logo-container">
+        
+         <img src={SJRStateLogo}/>
+        </div>
+        <div className="drop-down-buttons-container">
+         {lists.map((link) => <DropDownLink data={link}/>)}
+        </div>
+      </div>
+      <div className="jumbotron-container"></div>
+     
     </div>
   );
 }
---
+++ src/components/DropDownLink/DropDownLink.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./DropDownLink.css"
+import { useRef, useEffect } from 'react'
+
+
+const DropDownLink = (props) => {
+
+
+
+
+
+
+
+
+    return ( <div className="drop-down-button-wrapper" >
+         <button className="drop-down-button">{props.data.header}</button>
+            <div className="drop-down-menu-container" >
+                {props.data.links.map((link) =>   <div className="drop-down-link-container">
+
+<div className="link-circle"></div>
+<div className="link-text">{link}</div>
+</div>)}
+            
+            </div>
+    </div> );
+}
+ 
+export default DropDownLink;
